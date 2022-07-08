c70d9bb5372d2ec5c6a4264f3c11ac3bbf7e2ce6
paolopu91 <paolo.pugliese1991@gmail.com>
2022-07-08 10:35:02 +0200

    eserczio girono 2

---
+++ index2.php
@@ -0,0 +1,17 @@
+
+<?php 
+
+// le chiavi in questo array sono gli indici (0,1,2 etc...)
+$array=["pane", "pasta" , "farina"];
+
+echo $array [1];
+
+// in questo array le chiavi le abbiamo date noi in modo custom
+$arrayCustom =[
+    "chiave1" =>"pane",
+    "chiave2" =>"pasta",
+    "chiave3" =>"farina",
+    "chiave4" =>"uova",
+];
+
+?>
